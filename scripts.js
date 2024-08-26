8f9739316b2bffb5e93d5345dfcbcc9a61e64ae6
Shahrzad <shahrzadjkoushesh@gmail.com>
2024-08-26 20:24:02 +0330

    add basic html and js, no dynamic input is given

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function checkOddEven(limit) {
+    for (let i = 1; i <= limit; i++) {
+        if (i % 2 === 0) {
+            console.log(i + " EVEN");
+        } else {
+            console.log(i + " ODD");
+        }
+    }
+}
+
+let input = 10; 
+checkOddEven(input);
