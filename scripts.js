14510c42447b4115c94ed0bc9e939f1b6deba7d1
Shahrzad <shahrzadjkoushesh@gmail.com>
2024-08-28 16:16:28 +0330

    change input to number

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function checkOddEven() {
     let input = document.getElementById("user-input").value;
-    for (let i = 0; i <= input; i++) {
+    for (let i = 0; i <= +input; i++) {
         if (i % 2 === 0) {
             console.log(i + " EVEN");
         } else {
